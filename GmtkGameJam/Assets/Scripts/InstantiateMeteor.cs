39b1e8a77d504a68c4b908aac8b25a648016f31b
Said Hoca <saidburhan@gmail.com>
2020-07-11 21:50:38 +0300

    postproccessing sonrası
    post şeysi sonrası

--- GmtkGameJam/Assets/Scripts/InstantiateMeteor.cs
+++ GmtkGameJam/Assets/Scripts/InstantiateMeteor.cs
@@ -8,34 +8,42 @@ public class InstantiateMeteor : MonoBehaviour
     public Transform target;
     public Transform [] points;
     public GameObject[] meteorsPrefab;
-    public bool yarat = false;
+    [HideInInspector]public bool yarat = true;
+    public bool start = false;
     public int indisPoints,indisMeteors;
+    [HideInInspector] public float waitTime = 1.5f;
+
 
     public static InstantiateMeteor instance;
 
 	private void Awake()
 	{
         MakeInstance();
+
 	}
+
+	private void Start()
+	{
+        StartCoroutine(InstantiateMeteors());
+    }
 
 	void MakeInstance()
 	{
         if (instance == null) instance = this;
 	}
- 
-    void Update()
-    {
-        if(yarat)
-		{
-            InstantiateMeteors();
-            yarat = false;
-		}
-    }
 
-    public void InstantiateMeteors()
-	{
-		indisPoints = Random.Range(0, 8);
-        indisMeteors = Random.Range(0,4);
-        GameObject meteor = Instantiate(meteorsPrefab[indisMeteors],points[indisPoints].position, Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));                
-	}
+	private IEnumerator InstantiateMeteors()
+    {
+        Debug.Log("bu oldu");
+        while (yarat)
+        
+        Debug.Log("buda oldu");
+        indisPoints = Random.Range(0, 8);
+        indisMeteors = Random.Range(0, 4);
+        GameObject meteor = Instantiate(meteorsPrefab[indisMeteors], points[indisPoints].position, Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
+        yield return new WaitForSeconds(waitTime);
+        StartCoroutine(InstantiateMeteors());
+           
+        
+    }
 }
